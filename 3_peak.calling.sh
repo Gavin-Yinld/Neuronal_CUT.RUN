a163cb84f8336e1d93441c53d55b47836bee8462
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2022-12-10 15:51:55 +0800

    Add files via upload

---
+++ 3_peak.calling.sh
@@ -0,0 +1,67 @@
+#!bin/bash
+
+########################################
+####H3K27ac rep1
+bam=H3K27ac-exc-rep1.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K27ac-exc-rep1 -B --outdir ./ 
+
+bam=H3K27ac-inh-rep1.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K27ac-inh-rep1 -B --outdir ./ 
+####H3K27ac rep2
+bam=H3K27ac-exc-rep2.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K27ac-exc-rep2 -B --outdir ./ 
+
+bam=H3K27ac-inh-rep2.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K27ac-inh-rep2 -B --outdir ./ 
+########################################
+####H3K4me3 rep1
+bam=H3K4me3-exc-rep1.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K4me3-exc-rep1 -B --outdir ./ 
+
+bam=H3K4me3-inh-rep1.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K4me3-inh-rep1 -B --outdir ./ 
+####H3K4me3 rep2
+bam=H3K4me3-exc-rep2.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K4me3-exc-rep2 -B --outdir ./ 
+
+bam=H3K4me3-inh-rep2.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n H3K4me3-inh-rep2 -B --outdir ./ 
+########################################
+####H3K4me1 rep1
+bam=H3K4me1-exc-rep1.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K4me1-exc-rep1 -B --outdir ./ 
+
+bam=H3K4me1-inh-rep1.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K4me1-inh-rep1 -B --outdir ./ 
+####H3K4me1 rep2
+bam=H3K4me1-exc-rep2.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K4me1-exc-rep2 -B --outdir ./ 
+
+bam=H3K4me1-inh-rep2.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K4me1-inh-rep2 -B --outdir ./ 
+########################################
+####H3K27me3 rep1
+bam=H3K27me3-exc-rep1.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K27me3-exc-rep1 -B --outdir ./ 
+
+bam=H3K27me3-inh-rep1.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K27me3-inh-rep1 -B --outdir ./ 
+####H3K27me3 rep2
+bam=H3K27me3-exc-rep2.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K27me3-exc-rep2 -B --outdir ./ 
+
+bam=H3K27me3-inh-rep2.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K27me3-inh-rep2 -B --outdir ./ 
+########################################
+####Transcription factor EGR1 rep1
+bam=EGR1-exc-rep1.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-exc-rep1 -B --outdir ./ 
+
+bam=EGR1-inh-rep1.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-inh-rep1 -B --outdir ./ 
+####Transcription factor EGR1 rep2
+bam=EGR1-exc-rep2.rmdup.q30.bam # data for excitatory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-exc-rep2 -B --outdir ./ 
+
+bam=EGR1-inh-rep2.rmdup.q30.bam # data for inhibitory neurons
+macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-inh-rep2 -B --outdir ./ 
---
+++ 4_reproducible.peaks.sh
@@ -0,0 +1,87 @@
+#!bin/bash
+
+########################################
+####H3K27ac 
+rep1=H3K27ac-exc-rep1_peaks.narrowPeak  # data for excitatory neuron
+rep2=H3K27ac-exc-rep2_peaks.narrowPeak
+sorted_rep1=H3K27ac-exc-rep1_peaks.sorted.narrowPeak
+sorted_rep2=H3K27ac-exc-rep2_peaks.sorted.narrowPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type narrowPeak --rank signal.value --output-file H3K27ac-exc_idr --plot --log-output-file H3K27ac-exc.idr.log
+
+rep1=H3K27ac-inh-rep1_peaks.narrowPeak  # data for inhitatory neuron
+rep2=H3K27ac-inh-rep2_peaks.narrowPeak
+sorted_rep1=H3K27ac-inh-rep1_peaks.sorted.narrowPeak
+sorted_rep2=H3K27ac-inh-rep2_peaks.sorted.narrowPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type narrowPeak --rank signal.value --output-file H3K27ac-inh_idr --plot --log-output-file H3K27ac-inh.idr.log
+########################################
+####H3K4me3 
+rep1=H3K4me3-exc-rep1_peaks.narrowPeak  # data for excitatory neuron
+rep2=H3K4me3-exc-rep2_peaks.narrowPeak
+sorted_rep1=H3K4me3-exc-rep1_peaks.sorted.narrowPeak
+sorted_rep2=H3K4me3-exc-rep2_peaks.sorted.narrowPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type narrowPeak --rank signal.value --output-file H3K4me3-exc_idr --plot --log-output-file H3K4me3-exc.idr.log
+
+rep1=H3K4me3-inh-rep1_peaks.narrowPeak  # data for inhitatory neuron
+rep2=H3K4me3-inh-rep2_peaks.narrowPeak
+sorted_rep1=H3K4me3-inh-rep1_peaks.sorted.narrowPeak
+sorted_rep2=H3K4me3-inh-rep2_peaks.sorted.narrowPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type narrowPeak --rank signal.value --output-file H3K4me3-inh_idr --plot --log-output-file H3K4me3-inh.idr.log
+########################################
+####H3K4me1 
+rep1=H3K4me1-exc-rep1_peaks.broadPeak  # data for excitatory neuron
+rep2=H3K4me1-exc-rep2_peaks.broadPeak
+sorted_rep1=H3K4me1-exc-rep1_peaks.sorted.broadPeak
+sorted_rep2=H3K4me1-exc-rep2_peaks.sorted.broadPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type broadPeak --rank signal.value --output-file H3K4me1-exc_idr --plot --log-output-file H3K4me1-exc.idr.log
+
+rep1=H3K4me1-inh-rep1_peaks.broadPeak  # data for inhitatory neuron
+rep2=H3K4me1-inh-rep2_peaks.broadPeak
+sorted_rep1=H3K4me1-inh-rep1_peaks.sorted.broadPeak
+sorted_rep2=H3K4me1-inh-rep2_peaks.sorted.broadPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type broadPeak --rank signal.value --output-file H3K4me1-inh_idr --plot --log-output-file H3K4me1-inh.idr.log
+########################################
+####H3K27me3 
+rep1=H3K27me3-exc-rep1_peaks.broadPeak  # data for excitatory neuron
+rep2=H3K27me3-exc-rep2_peaks.broadPeak
+sorted_rep1=H3K27me3-exc-rep1_peaks.sorted.broadPeak
+sorted_rep2=H3K27me3-exc-rep2_peaks.sorted.broadPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type broadPeak --rank signal.value --output-file H3K27me3-exc_idr --plot --log-output-file H3K27me3-exc.idr.log
+
+rep1=H3K27me3-inh-rep1_peaks.broadPeak  # data for inhitatory neuron
+rep2=H3K27me3-inh-rep2_peaks.broadPeak
+sorted_rep1=H3K27me3-inh-rep1_peaks.sorted.broadPeak
+sorted_rep2=H3K27me3-inh-rep2_peaks.sorted.broadPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type broadPeak --rank signal.value --output-file H3K27me3-inh_idr --plot --log-output-file H3K27me3-inh.idr.log
+########################################
+####Transcription factor EGR1 
+rep1=EGR1-exc-rep1_peaks.narrowPeak  # data for excitatory neuron
+rep2=EGR1-exc-rep2_peaks.narrowPeak
+sorted_rep1=EGR1-exc-rep1_peaks.sorted.narrowPeak
+sorted_rep2=EGR1-exc-rep2_peaks.sorted.narrowPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type narrowPeak --rank signal.value --output-file EGR1-exc_idr --plot --log-output-file EGR1-exc.idr.log
+
+rep1=EGR1-inh-rep1_peaks.narrowPeak  # data for inhitatory neuron
+rep2=EGR1-inh-rep2_peaks.narrowPeak
+sorted_rep1=EGR1-inh-rep1_peaks.sorted.narrowPeak
+sorted_rep2=EGR1-inh-rep2_peaks.sorted.narrowPeak
+sort -k7,7nr $rep1 > $sorted_rep1
+sort -k7,7nr $rep2 > $sorted_rep2
+idr --samples $sorted_rep1 $sorted_rep2 --input-file-type narrowPeak --rank signal.value --output-file EGR1-inh_idr --plot --log-output-file EGR1-inh.idr.log
