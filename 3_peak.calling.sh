02374a3ed0afa7a8ccc71479e7f2af3817e845ca
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2022-12-10 16:02:39 +0800

    Update 3_peak.calling.sh

--- 3_peak.calling.sh
+++ 3_peak.calling.sh
@@ -55,13 +55,13 @@ macs2 callpeak -t $bam -g mm -f BAM --broad -p 0.05 -n H3K27me3-inh-rep2 -B --ou
 ########################################
 ####Transcription factor EGR1 rep1
 bam=EGR1-exc-rep1.rmdup.q30.bam # data for excitatory neurons
-macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-exc-rep1 -B --outdir ./ 
+macs2 callpeak -t $bam -g mm -f BAM -p 0.005 -n EGR1-exc-rep1 -B --outdir ./ 
 
 bam=EGR1-inh-rep1.rmdup.q30.bam # data for inhibitory neurons
-macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-inh-rep1 -B --outdir ./ 
+macs2 callpeak -t $bam -g mm -f BAM -p 0.005 -n EGR1-inh-rep1 -B --outdir ./ 
 ####Transcription factor EGR1 rep2
 bam=EGR1-exc-rep2.rmdup.q30.bam # data for excitatory neurons
-macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-exc-rep2 -B --outdir ./ 
+macs2 callpeak -t $bam -g mm -f BAM -p 0.005 -n EGR1-exc-rep2 -B --outdir ./ 
 
 bam=EGR1-inh-rep2.rmdup.q30.bam # data for inhibitory neurons
-macs2 callpeak -t $bam -g mm -f BAM -p 0.05 -n EGR1-inh-rep2 -B --outdir ./ 
+macs2 callpeak -t $bam -g mm -f BAM -p 0.005 -n EGR1-inh-rep2 -B --outdir ./ 
