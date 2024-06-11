7ca101cb72bc5fadb296b87bf6955706c181ba38
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2024-06-11 10:37:07 -0400

    Delete 5_Figures directory

--- 5_Figures/Figure2/Figure2B/EXC/EXC.sh
+++
@@ -1,7 +0,0 @@
-cd Figure2/Figure2B
-################################################################################################
-input=model_8.txt
-output=./
-jar=ChromHMM.jar  ##  software need to be downloaded
-java -jar $jar Reorder -f columnorderingfile -o EXC_stateorderingfile $input $output
-
